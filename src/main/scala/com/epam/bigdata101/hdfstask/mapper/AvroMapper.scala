0f6df92288c1d4b5aa53e2a4d07ea1dc87f5fcba
Artsiom_Chuiko <Artsiom_Chuiko@EPBYMINW0192.minsk.epam.com>
2018-10-31 18:02:58 +0300

    add avro mapper

---
+++ src/main/scala/com/epam/bigdata101/hdfstask/mapper/AvroMapper.scala
@@ -0,0 +1,26 @@
+package com.epam.bigdata101.hdfstask.mapper
+
+import org.apache.avro.Schema
+import org.apache.avro.Schema.Type
+import org.apache.avro.generic.{GenericData, GenericRecord}
+import org.apache.hadoop.io._
+import org.apache.hadoop.mapreduce.Mapper
+
+import scala.collection.JavaConverters._
+
+class AvroMapper extends Mapper[LongWritable, Text, Void, GenericRecord]{
+  val record = new GenericData.Record(Schema.create(Type.BYTES))
+
+  override def map(key: LongWritable, value: Text,
+                   context: Mapper[LongWritable, Text, Void, GenericRecord]#Context): Unit =
+    if (key.get() != 0) {
+      val s = Schema.create(Type.BYTES)
+      val names = s.getFields.asScala.map(_.name())
+      val values = value.toString.split(",")
+      values.zipWithIndex.foreach {
+        case (v, i) => record.put(names(i), v)
+      }
+
+      context.write(null, record)
+    }
+}
