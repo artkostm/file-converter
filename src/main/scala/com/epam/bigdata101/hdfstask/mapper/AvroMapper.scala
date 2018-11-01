8fcc2929cffab51e50a2d3ded1cd8f042f6e3b38
Artsiom_Chuiko <Artsiom_Chuiko@EPBYMINW0192.minsk.epam.com>
2018-11-01 12:29:36 +0300

    update avro mapper

--- src/main/scala/com/epam/bigdata101/hdfstask/mapper/AvroMapper.scala
+++ src/main/scala/com/epam/bigdata101/hdfstask/mapper/AvroMapper.scala
@@ -1,7 +1,9 @@
 package com.epam.bigdata101.hdfstask.mapper
 
+import java.io.File
+
+import com.epam.bigdata101.hdfstask.AppConfig
 import org.apache.avro.Schema
-import org.apache.avro.Schema.Type
 import org.apache.avro.generic.{GenericData, GenericRecord}
 import org.apache.hadoop.io._
 import org.apache.hadoop.mapreduce.Mapper
@@ -9,14 +11,15 @@ import org.apache.hadoop.mapreduce.Mapper
 import scala.collection.JavaConverters._
 
 class AvroMapper extends Mapper[LongWritable, Text, Void, GenericRecord]{
-
+  private val schemaParser = new Schema.Parser()
 
   override def map(key: LongWritable, value: Text,
                    context: Mapper[LongWritable, Text, Void, GenericRecord]#Context): Unit =
     if (key.get() != 0L) {
-      val record = new GenericData.Record(Schema.create(Type.BYTES))
-      val s = Schema.create(Type.BYTES)
-      val names = s.getFields.asScala.map(_.name())
+      val schemaPath = context.getConfiguration.get(AppConfig.SchemaFilePathKey)
+      val schema = schemaParser.parse(new File(schemaPath))
+      val record = new GenericData.Record(schema)
+      val names = schema.getFields.asScala.map(_.name())
       val values = value.toString.split(",")
       values.zipWithIndex.foreach {
         case (v, i) => record.put(names(i), v)
