f1afc4a41a47d7a542d1beaefc3c421297c0203c
Artsiom_Chuiko <Artsiom_Chuiko@EPBYMINW0192.minsk.epam.com>
2018-11-02 12:58:35 +0300

    reformat files

--- src/main/scala/com/epam/bigdata101/hdfstask/AppConfig.scala
+++ src/main/scala/com/epam/bigdata101/hdfstask/AppConfig.scala
@@ -9,24 +9,24 @@ import org.apache.hadoop.mapred.JobConf
 
 object ConverterType {
   sealed trait Converter
-  case object Avro extends Converter
+  case object Avro    extends Converter
   case object Parquet extends Converter
 }
 
 final case class AppConfig(
-                                inputFile: Path = new Path(Path.CUR_DIR),
-                                outputFile: Path = new Path(Path.CUR_DIR),
-                                schemaFile: String = Path.CUR_DIR,
-                                converter: ConverterType.Converter = new ConverterType.Converter {},
-                                skipHeaders: Boolean = true
-                              ) {
-  lazy val jobConfiguration: JobConf = new JobConf()
+    inputFile: Path = new Path(Path.CUR_DIR),
+    outputFile: Path = new Path(Path.CUR_DIR),
+    schemaFile: String = Path.CUR_DIR,
+    converter: ConverterType.Converter = new ConverterType.Converter {},
+    skipHeaders: Boolean = true,
+    jobConfiguration: JobConf = new JobConf()
+) {
   lazy val hdfs: FileSystem = FileSystem.get(jobConfiguration)
 }
 
 object AppConfig {
   private val logger = LogFactory.getLog(AppConfig.getClass)
-  val SkipHeaderKey = "hdfstask.csv.skip.header"
+  val SkipHeaderKey  = "hdfstask.csv.skip.header"
 
   private def printJobConfig(jobConf: Configuration): Unit = {
     val writer = new StringWriter()
@@ -36,7 +36,6 @@ object AppConfig {
 
   def print(appConfig: AppConfig): Unit = {
     logger.debug("Application configuration: " + AppConfig.unapply(appConfig).get)
-    println("Application configuration: " + AppConfig.unapply(appConfig).get)
     printJobConfig(appConfig.jobConfiguration)
   }
 }
@@ -46,22 +45,34 @@ object CliParser extends scopt.OptionParser[AppConfig]("file-converter") {
 
   head("File-Converter", "0.1")
 
-  opt[String]('i', "in").required().valueName("<file>").action((x, c) => c.copy(inputFile = new Path(x))).
-    text("in is a required file property")
+  opt[String]('i', "in")
+    .required()
+    .valueName("<file>")
+    .action((x, c) => c.copy(inputFile = new Path(x)))
+    .text("in is a required file property")
 
-  opt[String]('o', "out").required().valueName("<file>").action((x, c) => c.copy(outputFile = new Path(x))).
-    text("out is a required file property")
+  opt[String]('o', "out")
+    .required()
+    .valueName("<file>")
+    .action((x, c) => c.copy(outputFile = new Path(x)))
+    .text("out is a required file property")
 
-  opt[String]('s', "schema").required().valueName("<file>").validate(new File(_).exists() match {
-    case true => success
-    case false => failure("Schema file does not exist!")
-  }).action((x, c) => c.copy(schemaFile = x)).text("schema is a required file property")
+  opt[String]('s', "schema")
+    .required()
+    .valueName("<file>")
+    .validate(new File(_).exists() match {
+      case true  => success
+      case false => failure("Schema file does not exist!")
+    })
+    .action((x, c) => c.copy(schemaFile = x))
+    .text("schema is a required file property")
 
-  opt[Unit]('h', "header").action{(_, c) =>
-    c.jobConfiguration.setBoolean(AppConfig.SkipHeaderKey, false)
-    c.copy(skipHeaders = false)
-  }.text("header is a flag. if present, header will not be ignored")
-
+  opt[Unit]('h', "header")
+    .action { (_, c) =>
+      c.jobConfiguration.setBoolean(AppConfig.SkipHeaderKey, false)
+      c.copy(skipHeaders = false)
+    }
+    .text("header is a flag. if present, header will not be ignored")
 
   cmd("avro").action((_, c) => c.copy(converter = ConverterType.Avro)).text("avro is a command.")
 
@@ -69,7 +80,7 @@ object CliParser extends scopt.OptionParser[AppConfig]("file-converter") {
 
   checkConfig(_.converter match {
     case Avro | Parquet => success
-    case _ => failure("Please use either [avro] or [parquet] command!")
+    case _              => failure("Please use either [avro] or [parquet] command!")
   })
 
   checkConfig { c =>
--- src/main/scala/com/epam/bigdata101/hdfstask/FileConversionJob.scala
+++ src/main/scala/com/epam/bigdata101/hdfstask/FileConversionJob.scala
@@ -2,8 +2,10 @@ package com.epam.bigdata101.hdfstask
 
 import java.io.File
 
+import com.epam.bigdata101.hdfstask.ConverterType.{Avro, Parquet}
 import org.apache.avro.Schema
 import org.apache.avro.mapred.AvroOutputFormat
+import org.apache.avro.mapred.{AvroJob => MapredAvroJob}
 import org.apache.avro.mapreduce.AvroJob
 import org.apache.commons.logging.LogFactory
 import org.apache.hadoop.conf.Configured
@@ -11,6 +13,8 @@ import org.apache.hadoop.mapreduce.Job
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat
 import org.apache.hadoop.util.Tool
+import org.apache.parquet.avro.AvroParquetOutputFormat
+import org.apache.parquet.example.data.Group
 
 import scala.util.Try
 
@@ -24,35 +28,41 @@ object FileConversionJob extends Configured() with Tool with App {
   System.exit(code)
 
   override def run(args: Array[String]): Int = CliParser.parse(args, AppConfig()) match {
-    case Some(appConfig) => Try {
-      AppConfig.print(appConfig)
-      val jobConf = appConfig.jobConfiguration
-      val job = Job.getInstance(jobConf, getClass.getName)
-      job.setJarByClass(getClass)
+    case Some(appConfig) =>
+      Try {
+        AppConfig.print(appConfig)
+        val jobConf = appConfig.jobConfiguration
+        val job     = Job.getInstance(jobConf, getClass.getName)
+        job.setJarByClass(getClass)
 
-      jobConf.setOutputFormat(classOf[AvroOutputFormat[_]])
+        appConfig.converter match {
+          case Avro =>
+            job.setMapperClass(classOf[mapper.AvroMapper])
+            jobConf.setOutputFormat(classOf[AvroOutputFormat[_]])
+//          MapredAvroJob.setOutputCodec(jobConf, "snappy") //need to test this
+            AvroJob.setMapOutputValueSchema(job, new Schema.Parser().parse(new File(appConfig.schemaFile)))
+          case Parquet =>
+            job.setOutputValueClass(classOf[Group])
+            job.setOutputFormatClass(classOf[AvroParquetOutputFormat[_]])
+            AvroParquetOutputFormat.setSchema(job, new Schema.Parser().parse(new File(appConfig.schemaFile)))
+            job.setMapperClass(classOf[mapper.ParquetMapper])
+        }
 
-      job.setMapperClass(classOf[mapper.AvroMapper])
-      //job.setMapperClass(classOf[mapper.ParquetMapper])
-
-      AvroJob.setMapOutputValueSchema(job, new Schema.Parser().parse(new File(appConfig.schemaFile)))
-
-      job.setNumReduceTasks(0)
-
-      FileInputFormat.setInputPaths(job, appConfig.inputFile)
-      FileOutputFormat.setOutputPath(job, appConfig.outputFile)
-      job.waitForCompletion(true)
-    } match {
-      case scala.util.Success(res) =>
-        println("Success: " + res)
-        if (res) ExitCode.Success
-        else ExitCode.Failure
-      case scala.util.Failure(exception) =>
-        println(s"Unexpected error: ${exception.getMessage}")
-        exception.printStackTrace()
-        logger.error(s"Unexpected error: ${exception.getMessage}", exception)
-        ExitCode.Failure
-    }
+        job.setNumReduceTasks(0)
+        FileInputFormat.setInputPaths(job, appConfig.inputFile)
+        FileOutputFormat.setOutputPath(job, appConfig.outputFile)
+        job.waitForCompletion(true)
+      } match {
+        case scala.util.Success(res) =>
+          println("Success: " + res)
+          if (res) ExitCode.Success
+          else ExitCode.Failure
+        case scala.util.Failure(exception) =>
+          println(s"Unexpected error: ${exception.getMessage}")
+          exception.printStackTrace()
+          logger.error(s"Unexpected error: ${exception.getMessage}", exception)
+          ExitCode.Failure
+      }
     case None => ExitCode.InvalidInput
   }
 }
