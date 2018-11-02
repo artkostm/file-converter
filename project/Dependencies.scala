611f8a0018d48fc712530ce4612ab91522e8ecb5
Artsiom_Chuiko <artkostm@gmail.com>
2018-11-02 17:59:15 +0300

    add test parquet schema

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,19 +2,20 @@ import sbt._
 
 object Dependencies {
   val versions = new {
-    val hadoop       = "1.2.1"
-    val hadoopCommon = "3.1.1"
-    val scopt        = "3.7.0"
-    val mrunit       = "1.1.0"
-    val scalatest    = "3.0.5"
+    val hadoop        = "1.2.1"
+    val hadoopCommon  = "3.1.1"
+    val scopt         = "3.7.0"
+    val mrunit        = "1.1.0"
+    val scalatest     = "3.0.5"
+    val parquetHadoop = "1.10.0"
   }
 
   lazy val main = Seq(
-    "org.apache.hadoop"  % "hadoop-core"    % versions.hadoop % Provided intransitive (),
-    "org.apache.hadoop"  % "hadoop-common"  % versions.hadoopCommon % Provided,
-    "org.apache.avro"    % "avro-mapred"    % "1.8.2" classifier ("hadoop2"),
-    "org.apache.parquet" % "parquet-hadoop" % "1.10.0",
-    "com.github.scopt"   %% "scopt"         % versions.scopt
+    "org.apache.hadoop"  % "hadoop-core"           % versions.hadoop % Provided intransitive (),
+    "org.apache.hadoop"  % "hadoop-common"         % versions.hadoopCommon % Provided,
+    "org.apache.avro"    % "avro-mapred"           % "1.8.2" classifier ("hadoop2"),
+    "org.apache.parquet" % "parquet-hadoop-bundle" % versions.parquetHadoop,
+    "com.github.scopt"   %% "scopt"                % versions.scopt
   )
 
   lazy val test = Seq(
