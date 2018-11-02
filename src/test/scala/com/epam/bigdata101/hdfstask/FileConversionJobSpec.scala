7513aa81b802a83ddf4d8d551979740bbcc22e42
Artsiom_Chuiko <Artsiom_Chuiko@EPBYMINW0192.minsk.epam.com>
2018-11-02 18:06:45 +0300

    add parquet test case

--- src/test/scala/com/epam/bigdata101/hdfstask/FileConversionJobSpec.scala
+++ src/test/scala/com/epam/bigdata101/hdfstask/FileConversionJobSpec.scala
@@ -2,13 +2,17 @@ package com.epam.bigdata101.hdfstask
 
 import java.io.File
 
-import com.epam.bigdata101.hdfstask.mapper.AvroMapper
+import com.epam.bigdata101.hdfstask.mapper.{AvroMapper, ParquetMapper}
 import org.apache.avro.Schema
 import org.apache.avro.generic.GenericRecord
 import org.apache.avro.hadoop.io.AvroSerialization
 import org.apache.avro.mapred.AvroValue
 import org.apache.hadoop.io._
+import org.apache.hadoop.io.serializer.JavaSerialization
 import org.apache.hadoop.mrunit.mapreduce.MapDriver
+import org.apache.parquet.example.data.Group
+import org.apache.parquet.hadoop.example.GroupWriteSupport
+import org.apache.parquet.schema.MessageTypeParser
 import org.scalatest.FlatSpec
 
 class FileConversionJobSpec extends FlatSpec {
@@ -16,12 +20,25 @@ class FileConversionJobSpec extends FlatSpec {
   val values = List("12", "10.3", "some text")
 
   def withArvroMapDriver(schemaFile: String)
-                        (testCode: (MapDriver[LongWritable, Text, NullWritable, AvroValue[GenericRecord]]) => Unit): Unit = {
+                        (testCode: MapDriver[LongWritable, Text, NullWritable, AvroValue[GenericRecord]] => Unit): Unit = {
     val mapper = new AvroMapper
     val driver = MapDriver.newMapDriver(mapper)
     val config = driver.getConfiguration
     AvroSerialization.addToConfiguration(config)
     AvroSerialization.setValueWriterSchema(config, new Schema.Parser().parse(new File(schemaFile)))
+    testCode(driver)
+  }
+
+  def withParquetMapDriver(schemaFile: String)
+                          (testCode: MapDriver[LongWritable, Text, NullWritable, Group] => Unit): Unit = {
+    val mapper = new ParquetMapper
+    val driver = MapDriver.newMapDriver(mapper)
+    val config = driver.getConfiguration
+    val rawschema = FileConversionJob.readFile(schemaFile)
+    config.set(AppConfig.ParquetSchema, rawschema)
+    config.setStrings("io.serializations", config.get("io.serializations"),
+      classOf[JavaSerialization].getName())
+    GroupWriteSupport.setSchema(MessageTypeParser.parseMessageType(rawschema), config)
     testCode(driver)
   }
 
@@ -36,5 +53,15 @@ class FileConversionJobSpec extends FlatSpec {
       (0 until headers.size) foreach { i =>
         assert(value.get(0).getSecond.datum().get(headers(i)) == values(i))
       }
+  }
+
+  ignore should "convert input to parquet" in withParquetMapDriver("src/test/resources/testSchema.schema") {
+    driver =>
+      driver.addInput(new LongWritable(0), new Text(headers.mkString(",")))
+      driver.addInput(new LongWritable(1), new Text(values.mkString(",")))
+
+      val value = driver.run()
+
+      assert(value.size() == 1)
   }
 }
