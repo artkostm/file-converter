c25feb05342d6526884198fddfb832d4c8b6aca4
Artsiom_Chuiko <Artsiom_Chuiko@EPBYMINW0192.minsk.epam.com>
2018-11-02 18:06:22 +0300

    implement parquet mapper

--- src/main/scala/com/epam/bigdata101/hdfstask/mapper/AvroMapper.scala
+++ src/main/scala/com/epam/bigdata101/hdfstask/mapper/AvroMapper.scala
@@ -4,19 +4,30 @@ import org.apache.avro.Schema
 import org.apache.avro.generic.{GenericData, GenericRecord}
 import org.apache.avro.mapred.AvroValue
 import org.apache.hadoop.io._
+import org.apache.hadoop.mapreduce.Mapper
 
 import scala.collection.JavaConverters._
+import scala.collection.mutable
 
-class AvroMapper
-    extends HeaderSkippableMapper[LongWritable, Text, NullWritable, AvroValue[GenericRecord]]({ (_, value, context) =>
-      val schemaString = context.getConfiguration.get("avro.serialization.value.writer.schema")
-      val schema       = new Schema.Parser().parse(schemaString)
-      val record       = new GenericData.Record(schema)
-      val names        = schema.getFields.asScala.map(_.name())
-      val values       = value.toString.split(",")
-      values.zipWithIndex.foreach {
-        case (v, i) => record.put(names(i), v)
-      }
+class AvroMapper extends HeaderSkippableMapper[LongWritable, Text, NullWritable, AvroValue[GenericRecord]] {
+  private var schema: Schema                = _
+  private var names: mutable.Buffer[String] = _
 
-      context.write(NullWritable.get(), new AvroValue(record))
-    })
+  override def setup(context: Mapper[LongWritable, Text, NullWritable, AvroValue[GenericRecord]]#Context): Unit = {
+    val schemaString = context.getConfiguration.get("avro.serialization.value.writer.schema")
+    schema = new Schema.Parser().parse(schemaString)
+    names = schema.getFields.asScala.map(_.name())
+  }
+
+  override def process(key: LongWritable,
+                       value: Text,
+                       context: Mapper[LongWritable, Text, NullWritable, AvroValue[GenericRecord]]#Context): Unit = {
+    val record = new GenericData.Record(schema)
+    val values = value.toString.split(",")
+    values.zipWithIndex.foreach {
+      case (v, i) => record.put(names(i), v)
+    }
+
+    context.write(NullWritable.get(), new AvroValue(record))
+  }
+}
--- src/main/scala/com/epam/bigdata101/hdfstask/mapper/ParquetMapper.scala
+++ src/main/scala/com/epam/bigdata101/hdfstask/mapper/ParquetMapper.scala
@@ -1,8 +1,32 @@
 package com.epam.bigdata101.hdfstask.mapper
 
-import org.apache.avro.generic.GenericRecord
 import org.apache.hadoop.io.{LongWritable, NullWritable, Text}
+import org.apache.hadoop.mapreduce.Mapper
+import org.apache.parquet.example.data.{Group, GroupFactory}
+import org.apache.parquet.example.data.simple.SimpleGroupFactory
+import org.apache.parquet.hadoop.example.GroupWriteSupport
 
-class ParquetMapper
-    extends HeaderSkippableMapper[LongWritable, Text, NullWritable, GenericRecord]({ (key, value, context) =>
-      })
+import scala.collection.JavaConverters._
+
+class ParquetMapper extends HeaderSkippableMapper[LongWritable, Text, NullWritable, Group] {
+  private var groupFactory: GroupFactory = _
+
+  override def setup(context: Mapper[LongWritable, Text, NullWritable, Group]#Context): Unit =
+    groupFactory = new SimpleGroupFactory(GroupWriteSupport.getSchema(context.getConfiguration))
+
+  override def process(key: LongWritable,
+                       value: Text,
+                       context: Mapper[LongWritable, Text, NullWritable, Group]#Context): Unit = {
+    val values = value.toString.split(",")
+    val group  = groupFactory.newGroup()
+
+    val groupType = group.getType()
+    val names     = groupType.getFields.asScala.map(_.getName)
+
+    values.zipWithIndex.foreach {
+      case (v, i) => group.add(names(i), v)
+    }
+
+    context.write(NullWritable.get(), group)
+  }
+}
