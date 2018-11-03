46488ba5a6f8a277277b3da8659a1d0b96b6e099
Artsiom <artkostm@gmail.com>
2018-11-03 16:39:54 +0300

    update header mapper

--- src/main/scala/com/epam/bigdata101/hdfstask/config/ExitCode.scala
+++ src/main/scala/com/epam/bigdata101/hdfstask/config/ExitCode.scala
@@ -1,3 +1,7 @@
-package com.epam.bigdata101.hdfstask.config object ExitCode {
+package com.epam.bigdata101.hdfstask.config
 
+object ExitCode {
+  val Success      = 0
+  val InvalidInput = -1
+  val Failure      = -2
 }
--- src/main/scala/com/epam/bigdata101/hdfstask/mapper/HeaderSkippableMapper.scala
+++ src/main/scala/com/epam/bigdata101/hdfstask/mapper/HeaderSkippableMapper.scala
@@ -1,15 +1,51 @@
 package com.epam.bigdata101.hdfstask.mapper
 
-import com.epam.bigdata101.hdfstask.AppConfig
 import org.apache.hadoop.io.LongWritable
 import org.apache.hadoop.mapreduce.Mapper
 
-abstract class HeaderSkippableMapper[InKey <: LongWritable, InValue, OutKey, OutValue]
+/**
+  * An abstract mapper class which is able to skip header of a file
+  *
+  * By default, header will be ignored. To not ignore the header,
+  * set SkipHeaderKey property 'hdfstask.csv.skip.header' to false using the Configuration object
+  *
+  * @tparam InKey - input key type
+  * @tparam InValue - input value type
+  * @tparam OutKey - output key type
+  * @tparam OutValue - output value type
+  * @tparam SetupValue - result calling withSetup
+  */
+abstract class HeaderSkippableMapper[InKey <: LongWritable, InValue, OutKey, OutValue, SetupValue]
     extends Mapper[InKey, InValue, OutKey, OutValue] {
-  override def map(key: InKey, value: InValue, context: Mapper[InKey, InValue, OutKey, OutValue]#Context): Unit =
-    if (!context.getConfiguration.getBoolean(AppConfig.SkipHeaderKey, true) || key.get() != 0L) {
-      process(key, value, context)
-    }
+  import HeaderSkippableMapper._
 
-  def process(key: InKey, value: InValue, context: Mapper[InKey, InValue, OutKey, OutValue]#Context): Unit
+  private var setupMethod: Option[Mapper[InKey, InValue, OutKey, OutValue]#Context => SetupValue] = None
+  private var mapMethod
+    : Option[(InKey, InValue, Mapper[InKey, InValue, OutKey, OutValue]#Context, SetupValue) => Unit] = None
+  private var setupValue: SetupValue                                                                 = _
+
+  /**
+    * Helper function to setup Mapper
+    */
+  protected def withSetup(setupF: Mapper[InKey, InValue, OutKey, OutValue]#Context => SetupValue): Unit =
+    setupMethod = Some(setupF)
+
+  /**
+    * Helper function to process rows
+    */
+  protected def withMap(
+      mapF: (InKey, InValue, Mapper[InKey, InValue, OutKey, OutValue]#Context, SetupValue) => Unit): Unit =
+    mapMethod = Some(mapF)
+
+  override def setup(context: Mapper[InKey, InValue, OutKey, OutValue]#Context): Unit =
+    setupValue = setupMethod.map(_(context)).getOrElse(null.asInstanceOf[SetupValue])
+
+  override def map(key: InKey, value: InValue, context: Mapper[InKey, InValue, OutKey, OutValue]#Context): Unit =
+    if (!context.getConfiguration.getBoolean(SkipHeaderKey, true) || key.get() != 0L) {
+      mapMethod.map(_(key, value, context, setupValue)).orNull
+    }
+}
+
+object HeaderSkippableMapper {
+  val SkipHeaderKey = "hdfstask.csv.skip.header"
 }
