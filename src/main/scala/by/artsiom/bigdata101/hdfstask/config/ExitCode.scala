511cc729ff953be95e968841c44f67178eec03b0
Artsiom Chuiko <artkostm@gmail.com>
2018-11-05 12:48:08 +0300

    Update ExitCode.scala

--- src/main/scala/by/artsiom/bigdata101/hdfstask/config/ExitCode.scala
+++ src/main/scala/by/artsiom/bigdata101/hdfstask/config/ExitCode.scala
@@ -2,6 +2,6 @@ package by.artsiom.bigdata101.hdfstask.config
 
 object ExitCode {
   val Success      = 0
-  val InvalidInput = -1
-  val Failure      = -2
+  val InvalidInput = 1
+  val Failure      = 2
 }
