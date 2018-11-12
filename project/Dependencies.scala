7dca1e80dfc4c185936d15985d1f2052a6bb7ade
Artsiom Chuiko <Artsiom_Chuiko@epam.com>
2018-11-12 14:05:52 +0300

    update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,8 +2,7 @@ import sbt._
 
 object Dependencies {
   val versions = new {
-    val hadoop        = "1.2.1"
-    val hadoopCommon  = "3.1.1"
+    val hadoop        = "2.7.3"
     val scopt         = "3.7.0"
     val parquetHadoop = "1.10.0"
 
@@ -14,8 +13,7 @@ object Dependencies {
   }
 
   lazy val main = Seq(
-    "org.apache.hadoop"  % "hadoop-core"           % versions.hadoop % Provided intransitive (),
-    "org.apache.hadoop"  % "hadoop-common"         % versions.hadoopCommon % Provided,
+    "org.apache.hadoop"  % "hadoop-client"         % versions.hadoop % Provided,
     "org.apache.avro"    % "avro-mapred"           % "1.8.2" classifier ("hadoop2"),
     "org.apache.parquet" % "parquet-hadoop-bundle" % versions.parquetHadoop,
     "com.github.scopt"   %% "scopt"                % versions.scopt
@@ -23,10 +21,9 @@ object Dependencies {
 
   lazy val test = Seq(
     "org.scalatest"     %% "scalatest"  % versions.scalatest,
-    "org.apache.mrunit" % "mrunit"      % versions.mrunit classifier ("hadoop1"),
     "org.scalacheck"    %% "scalacheck" % versions.scalaCheck,
     "org.scalamock"     %% "scalamock"  % versions.scalaMock,
-    // have to add this dependency to be able to mock mapper context
-    "org.apache.hadoop" % "hadoop-common" % versions.hadoopCommon
+    "org.apache.mrunit" % "mrunit"      % versions.mrunit classifier ("hadoop1"),
+    "org.apache.mrunit" % "mrunit"      % versions.mrunit classifier ("hadoop2")
   ).map(_ % Test)
 }
