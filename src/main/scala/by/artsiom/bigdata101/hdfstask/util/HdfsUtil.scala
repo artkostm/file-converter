db003d4e54bba1f25157ff794a979affd6816da2
Artsiom Chuiko <Artsiom_Chuiko@epam.com>
2018-11-12 14:07:01 +0300

    small update

--- src/main/scala/by/artsiom/bigdata101/hdfstask/util/HdfsUtil.scala
+++ src/main/scala/by/artsiom/bigdata101/hdfstask/util/HdfsUtil.scala
@@ -4,8 +4,8 @@ import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.{FileSystem, Path}
 
 /**
-* Simple wrapper for testing purposes
-*/
+  * Simple wrapper for testing purposes
+  */
 class HdfsUtil(config: Configuration) {
 
   def exists(file: Path): Boolean = FileSystem.get(config).exists(file)
