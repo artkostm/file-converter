6d1d518cdd6eb35e2ce935941a2f36b0136c1d72
Artsiom Chuiko <Artsiom_Chuiko@epam.com>
2018-11-12 16:14:49 +0300

    Update ExitCode.scala

--- src/main/scala/by/artsiom/bigdata101/hdfstask/config/ExitCode.scala
+++ src/main/scala/by/artsiom/bigdata101/hdfstask/config/ExitCode.scala
@@ -1,7 +1,8 @@
 package by.artsiom.bigdata101.hdfstask.config
 
 object ExitCode {
-  val Success      = 0
-  val InvalidInput = 1
-  val Failure      = 2
+  val Success         = 0
+  val InvalidInput    = 1
+  val UnexpectedError = 2
+  val Failure         = 3
 }
